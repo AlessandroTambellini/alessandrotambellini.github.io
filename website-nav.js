ccfaa88fb97265c21411ed1f4e0175485d800328
Alessandro Tambellini <alessandrotambellini03@gmail.com>
2025-02-19 10:35:06 +0100

    refine the website

--- website-nav.js
+++ website-nav.js
@@ -1,14 +1,19 @@
+const website_nav = document.querySelector('#website-nav');
 const button = document.querySelector('#website-nav button');
 const ul = document.querySelector('#website-nav ul');
 button.addEventListener('click', toggle_ul);
 
-let toggle = true;
+let is_menu_closed = true;
 function toggle_ul() {
-    if (toggle) {
+    if (is_menu_closed) {
         ul.style.display = 'block';
+        website_nav.style.boxShadow = '0 0 5px rgb(237, 237, 237)';
+        website_nav.style.backgroundColor = 'rgb(18, 18, 18)';
     } else {
         ul.style.display = 'none';
+        website_nav.style.boxShadow = 'unset';
+        website_nav.style.backgroundColor = 'unset';
     }
 
-    toggle = !toggle;
+    is_menu_closed = !is_menu_closed;
 }
