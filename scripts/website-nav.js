fc43c6747fff8ac419ff9866508cbef67a61de1f
Alessandro Tambellini <alessandrotambellini03@gmail.com>
2025-08-26 12:34:41 +0200

    .

--- scripts/website-nav.js
+++ scripts/website-nav.js
@@ -1,19 +1,48 @@
 const website_nav = document.querySelector('#website-nav');
 const open_website_nav_btn = document.querySelector('#open-website-nav-btn');
+const minify_nav_btn = website_nav.querySelector('button[title="minify nav"]');
+const expand_nav_btn = website_nav.querySelector('button[title="expand nav"]');
 const main = document.querySelector('main');
+
+(function main()
+{
+    website_nav.switch_class          = 
+    open_website_nav_btn.switch_class = 
+    minify_nav_btn.switch_class       = 
+    expand_nav_btn.switch_class       = switch_class;
+    
+    website_nav.addEventListener('click', e => e.stopPropagation());
+    open_website_nav_btn.addEventListener('click', e => e.stopPropagation());
+})();
 
 open_website_nav_btn.addEventListener('click', () => 
 {    
-    website_nav.className = 'display-block';
-    open_website_nav_btn.className = 'display-none';
-    main.className = 'display-opaque';
+    website_nav.switch_class('display-none', 'display-block');
+    open_website_nav_btn.switch_class('display-block', 'display-none');
+    main.classList.add('display-opaque');
 });
 
-website_nav.addEventListener('click', e => e.stopPropagation());
-open_website_nav_btn.addEventListener('click', e => e.stopPropagation());
+minify_nav_btn.addEventListener('click', () => 
+{
+    website_nav.querySelector('ul').classList.add('minified-list');
+    minify_nav_btn.switch_class('display-block', 'display-none');
+    expand_nav_btn.switch_class('display-none', 'display-block');
+});
+
+expand_nav_btn.addEventListener('click', () => 
+{
+    website_nav.querySelector('ul').classList.remove('minified-list');
+    expand_nav_btn.switch_class('display-block', 'display-none');
+    minify_nav_btn.switch_class('display-none', 'display-block');
+});
 
 document.addEventListener('click', () => {
-    website_nav.className = 'display-none';
-    open_website_nav_btn.className = 'display-block';
-    main.className = '';
+    website_nav.switch_class('display-block', 'display-none'); 
+    open_website_nav_btn.switch_class('display-none', 'display-block');
+    main.classList.remove('display-opaque');
 });
+
+function switch_class(old, _new) {
+    this.classList.remove(old);
+    this.classList.add(_new)
+}
